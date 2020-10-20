2d03dd148b555ef5749910b3c1fb9604e82be6a7
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-10-20 13:02:22 +0530

    Code: Add boilerplate

---
+++ inc/below.php
@@ -0,0 +1,64 @@
+
+			<!-- ~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/ -->
+			<!-- Page-specific content goes here. -->
+			<!-- ~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/ -->
+
+		</div> <!-- END : Page Content -->
+
+
+		<!-- Lazaro Signature -->
+		<?php lazaro_signature(); ?>
+		<!-- END : Lazaro Signature -->
+
+	</div><!-- END : Page Wrapper -->
+
+	<?php require_once 'modals.php' ?>
+
+	<!--  ☠  MARKUP ENDS HERE  ☠  -->
+
+	<?php lazaro_disclaimer(); ?>
+
+
+
+
+
+
+
+
+
+	<!-- JS Modules -->
+	<script type="text/javascript" src="/js/modules/utils.js"></script>
+	<!-- <script type="text/javascript" src="/js/modules/device-charge.js"></script> -->
+	<script type="text/javascript" src="/js/modules/video_embed.js"></script>
+	<script type="text/javascript" src="/js/modules/modal_box.js"></script>
+	<script type="text/javascript" src="/js/modules/form.js"></script>
+	<script type="text/javascript" src="/js/modules/disclaimer.js"></script>
+
+	<script type="text/javascript">
+
+		$( function () {
+			//
+		} );
+
+	</script>
+
+	<!-- Other Modules -->
+	<?php // require __DIR__ . '/inc/can-user-hover.php' ?>
+
+
+	<?php
+		/*
+		 * Arbitrary Code ( Bottom of Body )
+		 */
+		echo getContent( '', 'arbitrary_code_body_bottom' );
+	?>
+
+	<?php /* Query Monitor CMS plugin */ ?>
+	<?php if ( CMS_ENABLED and is_user_logged_in() ) : ?>
+		<script type="text/javascript" src="cms/wp-content/plugins/query-monitor/assets/query-monitor.js<?= $ver ?>"></script>
+	<?php endif; ?>
+
+
+</body>
+
+</html>
