3997c9fa13c1c168dbb9a032e9f4a86a96e06118
MarioWindsor <mario@lazaro.in>
2020-10-22 16:29:40 +0530

    Front-end Build: Part 2

--- pages/home.php
+++ pages/home.php
@@ -28,23 +28,42 @@
 
 
 <!-- Intro Section -->
-<section class="intro-section space-50-top-bottom">
+<section class="intro-section fill-red-2 space-100-top-bottom">
 	<div class="container">
-		<div class="row">
-			<div class="intro fill-red-2 columns small-12 medium-6 large-8 space-50">
-				<div class="logo"><img class="block" src="../media/wh-logo-large-dark.svg<?php echo $ver ?>"></div>
-				<div class="description p space-50-top-bottom text-blue-4">Invest in rental-yielding real estate assets that are backed by a business model. Get the right returns, with managed volatility and capital protection.</div>
-				<div class="h5 strong">Curated rental-yielding assets, with</div>
-				<div class="h2 strong">Returns you cannot ignore. </div>
+		<div class="intro row">
+			<div class="columns small-12">
+				<div class="logo space-100-bottom"><img class="block" src="../media/wh-logo-large-dark.svg<?php echo $ver ?>"></div>
 			</div>
+			<div class="columns small-12 medium-10 large-4 inline-top">
+				<div class="description h5 line-height-large text-blue-4 space-50-bottom">Invest in rental-yielding real estate assets that are backed by a business model. Get the right returns, with managed volatility and capital protection.</div>
+			</div>
+			<div class="columns small-12 medium-7 large-5 large-offset-1 xlarge-4 inline-top">
+				<div class="h5 strong line-height-large space-min-bottom">Curated rental-yielding assets, with</div>
+				<div class="h1 strong">Returns you cannot ignore.</div>
+			</div>
+		</div>
+	</div>
+</section>
 
-			<div class="action fill-blue-4 columns small-12 medium-6 large-4 space-50">
-				<div class="heading h5 strong clearfix space-25-bottom">
-					<img class="float-right" src="../media/wh-logo-small-light.png<?php echo $ver ?>">
+
+<!-- Action Section -->
+<section class="action-section fill-blue-4 space-100-top-bottom">
+	<div class="container">
+		<div class="action row">
+			<div class="columns small-12 medium-9 large-6 large-offset-5 xlarge-5">
+				<div class="heading h3 strong line-height-medium clearfix space-50-bottom">
+					<img class="float-left" src="../media/wh-logo-small-red.svg<?php echo $ver ?>">
 					Why not invest in a 3BHK flat and get a 7% return on this investment? <span class="text-red-2">That’s a better return than a fixed deposit.</span>
 				</div>
-				<div class="image"><img class="block" src="../media/char-1.png<?php echo $ver ?>"></div>
-				<div class="form form-dark space-min-top space-25-bottom">
+			</div>
+			<div class="columns small-12 space-50-bottom">
+				<div class="image">
+					<img class="block hide-large hide-xlarge" src="../media/char-1-small.png<?php echo $ver ?>">
+					<img class="block hide-small hide-medium" src="../media/char-1-large.png<?php echo $ver ?>">
+				</div>
+			</div>
+			<div class="columns small-12 medium-6 large-4">
+				<div class="form form-dark space-25-bottom">
 					<div class="part-1">
 						<div class="form-row space-min-bottom">
 							<div class="label">
@@ -80,9 +99,11 @@
 						</div>
 					</div>
 				</div>
+			</div>
+			<div class="columns small-12 medium-5 medium-offset-1 large-4">
 				<div class="contact">
-					<div class="p text-blue-1 line-height-small space-min-bottom">Talk to an investment <br>manager today.</div>
-					<a class="h4 inline text-red-2" href="tel:">+91 98860 98860 </a>
+					<div class="h5 text-blue-1 space-min-bottom">Talk to an investment <br>manager today.</div>
+					<a class="h3 inline text-red-2" href="tel:">+91 98860 98860 </a>
 				</div>
 			</div>
 		</div>
@@ -92,39 +113,43 @@
 
 
 <!-- How Section -->
-<section class="how-section space-50-top space-100-bottom">
+<section class="how-section space-100-top-bottom">
 	<div class="container">
 		<div class="row">
-			<div class="how columns small-12 space-50">
+			<div class="how columns small-12 large-10">
 				<div class="h3 strong space-50-bottom">How does it work?</div>
 				<div class="table">
 					<div class="row table-head">
-						<div class="table-cell h4 fill-red-2 columns small-6 space-25">Lumpsum</div>
-						<div class="table-cell h4 fill-blue-4 columns small-6 space-25">EMI</div>
+						<div class="table-cell h5 fill-red-2 columns small-6 space-25 clearfix">Lumpsum <span class="float-right">+</span></div>
+						<div class="table-cell h5 fill-blue-4 columns small-6 space-25 clearfix">EMI <span class="float-right">+</span></div>
 					</div>
 					<div class="row table-row">
-						<div class="table-cell p fill-red-1 text-dark columns small-6 space-25">1. Invest in any of the listed residential properties. </div>
-						<div class="table-cell p fill-blue-2 text-dark columns small-6 space-25">1. Get a home loan approval for any of the listed residential properties.</div>
+						<div class="table-cell p fill-red-1 text-red-4 columns small-6 space-25"><span class="num strong">1.</span>Invest in any of the listed residential properties. </div>
+						<div class="table-cell p fill-blue-2 text-blue-4 columns small-6 space-25"><span class="num strong">1.</span>Get a home loan approval for any of the listed residential properties.</div>
 					</div>
 					<div class="row table-row">
-						<div class="table-cell p fill-red-1 text-dark columns small-6 space-25">2. The property is transferred to you via an absolute sale deed. </div>
-						<div class="table-cell p fill-blue-2 text-dark columns small-6 space-25">2. Pay the ‘Minimum Investment Amount’ as the downpayment for the home loan.</div>
+						<div class="table-cell p fill-red-1 text-red-4 columns small-6 space-25"><span class="num strong">2.</span>The property is transferred to you via an absolute sale deed. </div>
+						<div class="table-cell p fill-blue-2 text-blue-4 columns small-6 space-25"><span class="num strong">2.</span>Pay the ‘Minimum Investment Amount’ as the downpayment for the home loan.</div>
 					</div>
 					<div class="row table-row">
-						<div class="table-cell p fill-red-1 text-dark columns small-6 space-25">3. Sign a 9-year rental agreement.</div>
-						<div class="table-cell p fill-blue-2 text-dark columns small-6 space-25">3. Complete the home loan paperwork and formalities. </div>
+						<div class="table-cell p fill-red-1 text-red-4 columns small-6 space-25"><span class="num strong">3.</span>Sign a 9-year rental agreement.</div>
+						<div class="table-cell p fill-blue-2 text-blue-4 columns small-6 space-25"><span class="num strong">3.</span>Complete the home loan paperwork and formalities. </div>
 					</div>
 					<div class="row table-row">
-						<div class="table-cell p fill-red-1 text-dark columns small-6 space-25">4. Get a fixed yearly rental income for 9 years.</div>
-						<div class="table-cell p fill-blue-2 text-dark columns small-6 space-25">4. Sign a 9-year rental agreement.</div>
+						<div class="table-cell p fill-red-1 text-red-4 columns small-6 space-25"><span class="num strong">4.</span>Get a fixed yearly rental income for 9 years.</div>
+						<div class="table-cell p fill-blue-2 text-blue-4 columns small-6 space-25"><span class="num strong">4.</span>Sign a 9-year rental agreement.</div>
 					</div>
 					<div class="row table-row">
-						<div class="table-cell p fill-red-1 text-dark columns small-6 space-25">&nbsp;</div>
-						<div class="table-cell p fill-blue-2 text-dark columns small-6 space-25">5. Get a fixed yearly rental income for 9 years.</div>
+						<div class="table-cell p fill-red-1 text-red-4 columns small-6 space-25">&nbsp;</div>
+						<div class="table-cell p fill-blue-2 text-blue-4 columns small-6 space-25"><span class="num strong">5.</span>Get a fixed yearly rental income for 9 years.</div>
 					</div>
 					<div class="row table-row">
-						<div class="table-cell p fill-red-1 text-dark columns small-6 space-25">&nbsp;</div>
-						<div class="table-cell p fill-blue-2 text-dark columns small-6 space-25">6. The rental income services your monthly EMI’s. </div>
+						<div class="table-cell p fill-red-1 text-red-4 columns small-6 space-25">&nbsp;</div>
+						<div class="table-cell p fill-blue-2 text-blue-4 columns small-6 space-25"><span class="num strong">6.</span>The rental income services your monthly EMI’s. </div>
+					</div>
+					<div class="row table-foot">
+						<div class="table-cell text-red-2 text-right text-uppercase label space-min columns small-6">Show Details</div>
+						<div class="table-cell text-blue-4 text-right text-uppercase label space-min columns small-6">Show Details</div>
 					</div>
 				</div>
 			</div>
@@ -138,7 +163,7 @@
 <section class="about-section fill-blue-4 space-100-top-bottom">
 	<div class="container">
 		<div class="row">
-			<div class="about columns small-12 large-10 xlarge-8 space-50">
+			<div class="about columns small-12 large-10 xlarge-8">
 				<div class="logo"><img class="block" src="../media/wh-logo-large-red.svg<?php echo $ver ?>"></div>
 				<div class="p text-blue-1 space-50-top">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit quas dignissimos est suscipit ratione consequatur debitis sequi, molestiae quasi, esse, aliquam quibusdam necessitatibus vel accusantium alias, cum. Molestiae, mollitia, voluptatem.</div>
 			</div>
