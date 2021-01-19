dff7b4705c7bf93c583fb86b23e63b8a751b8c8b
adi <52442230+adi-lazaro@users.noreply.github.com>
2021-01-19 11:52:52 +0530

    FAQs: Develop a way to output a list of all the FAQs as a hierarchy

--- pages/faq.php
+++ pages/faq.php
@@ -3,8 +3,35 @@
 // Page-specific preparatory code goes here.
 require_once __DIR__ . '/../inc/above.php';
 
-// If no free-form post content was provided, use the summary
-$thePost->set( 'content', wp_strip_all_tags( $thePost->get( 'post_content' ) ) ?: $thePost->get( 'summary' ) );
+$faqs = BFS\CMS::getPostsOf( 'faq' );
+$faqs__Tree = [ ];
+foreach ( $faqs as $faq ) {
+	$faq->set( 'url', get_permalink( $faq->get( 'ID' ) ) );
+	$faq->set( 'featuredImage', get_the_post_thumbnail_url( $faq->get( 'ID' ) ) );
+	// If no free-form post content was provided, use the summary
+	$faq->set( 'content', wp_strip_all_tags( $faq->get( 'post_content' ) ) ?: $faq->get( 'summary' ) );
+	// Build the a hierarchical tree representation of all the FAQs
+	$faqs__Tree[ $faq->get( 'post_parent' ) ][ ] = $faq;
+}
+
+function getFAQHierarchyMarkup ( $faqs__Tree, $parentId ) {
+	if ( empty( $faqs__Tree[ $parentId ] ) )
+		return '';
+
+	?>
+
+	<ul>
+		<?php foreach ( $faqs__Tree[ $parentId ] as $faq ) : ?>
+			<li>
+				<a href="<?= $faq->get( 'url' ) ?>" target="_blank"><?= $faq->get( 'post_title' ) ?></a>
+				<?= getFAQHierarchyMarkup( $faqs__Tree, $faq->get( 'ID' ) ) ?>
+			</li>
+		<?php endforeach; ?>
+	<ul>
+
+	<?php
+
+}
 
 ?>
 
@@ -12,6 +39,11 @@ $thePost->set( 'content', wp_strip_all_tags( $thePost->get( 'post_content' ) ) ?
 
 <section class="document-section space-50-bottom">
 	<div class="container">
+		<div class="row">
+			<div class="columns small-6 medium-6 medium-offset-1 xlarge-6 xlarge-offset-2 space-min">
+				<?= getFAQHierarchyMarkup( $faqs__Tree, 0 ) ?>
+			</div>
+		</div>
 		<div class="row">
 			<div class="columns small-12 medium-10 medium-offset-1 xlarge-8 xlarge-offset-2 space-min">
 				<h3><?= $thePost->get( 'post_title' ) ?></h3>
