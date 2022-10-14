cd8b0775fca40bbdacd12bf68fee89e0e31837bd
adi <52442230+adi-lazaro@users.noreply.github.com>
2022-10-14 12:18:27 +0530

    BUG: CMS: WordPress: Gutenberg: Load custom script after certain dependencies

--- cms/wp-content/themes/braun/settings/gutenberg-block-editor.php
+++ cms/wp-content/themes/braun/settings/gutenberg-block-editor.php
@@ -9,7 +9,7 @@ add_action( 'enqueue_block_editor_assets', function () {
 	wp_enqueue_script(
 		'bfs-guten-script',
 		get_template_directory_uri() . '/js/blocks.js',
-		[ 'wp-dom-ready', 'wp-blocks', 'wp-edit-post' ],
+		[ 'wp-dom-ready', 'wp-editor', 'wp-hooks', 'wp-blocks', 'wp-edit-post' ],
 		filemtime( get_template_directory() . '/js/blocks.js' )
 	);
 } );
